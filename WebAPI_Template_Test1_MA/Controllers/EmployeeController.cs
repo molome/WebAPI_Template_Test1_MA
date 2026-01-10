be2226df7d27b5a3bcd318c6fde7eb8d32758a7d
mohsin.ahmed <mohsin.ahmed@AP-69G3PN2>
2026-01-10 12:54:49 +0530

    Add project files.

---
+++ WebAPI_Template_Test1_MA/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_Template_Test1_MA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        List<string> employeeNameList = new List<string>()
+        {
+            "Mohsin Ahmed",
+            "Mohammed Mohalil",
+            "Zakir Hussain"
+        };
+
+        Dictionary<int, string> empList = new Dictionary<int, string>()
+        {
+            {1, "Mohsin Ahmed" },
+            {2, "Mohammed Mohalil" },
+            {3, "Zakir Hussain" }
+        };
+
+        [HttpGet]
+        [Route("GetAllEmployees")]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(employeeNameList);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            
+            return Ok(empList.GetValueOrDefault(id));
+        }
+
+    }
+}
