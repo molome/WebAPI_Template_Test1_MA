e266f97163db97b989daa729a227c49c1f1a7f02
Mohsin Ahmed <mohsinahmed5626@gmail.com>
2026-01-15 16:18:36 +0530

    Added swagger UI globally

--- WebAPI_Template_Test1_MA/Program.cs
+++ WebAPI_Template_Test1_MA/Program.cs
@@ -26,9 +26,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
